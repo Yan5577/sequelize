d261fe6cac10f704dc037f7a94a12e2c780538cb
Yan5577 <zuoyan775@gmai.com>
2022-11-16 11:13:52 +0000

    sequlize code create

---
+++ src/movie/function.js
@@ -0,0 +1,13 @@
+const Movie = require("./table");
+
+async function createMovie(movieObject) {
+  try {
+    const newMovie = await Movie.create(movieObject);
+    console.log(newMovie);
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+
+exports.createMovie = createMovie;
